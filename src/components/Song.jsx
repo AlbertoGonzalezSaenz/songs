5052386c9b8b19a174741499eb33ff93f81aad21
AlbertoGonzalezSaenz <alberto.gonzalez@kinandcarta.com>
2022-01-25 12:19:21 -0500

    connected React to Redux

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
-const Song = ({title, duration}) => {
+const Song = ({song, songs, selectSong}) => {
     return (
         <div className="item" >
             <div className="right floated content">
-                <button className="ui button primary">
+                <button 
+                    className="ui button primary"
+                    onClick={() => selectSong(song)}
+                >
                     Select
                 </button>
             </div>
-            <div className="content">{title}</div>
+            <div className="content">{song.title}</div>
         </div>
     );
 }
+
 
 export default Song;
--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { selectSong } from '../actions';
 import Song from './Song';
 
-const SongList = ({ songs }) => {
-    const renderedSongs = songs.map(song => <Song key={song.title} {...song}/>)
+const SongList = ({ songs, selectSong }) => {
+    const renderedSongs = songs.map(song => <Song key={song.title} song={song} selectSong={selectSong}/>)
 
     return (
         <div className="ui devided list">
@@ -13,8 +14,8 @@ const SongList = ({ songs }) => {
 }
 
 const mapStateToProps = (state) => {
-
+    console.log(state)
     return { songs: state.songs }
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
